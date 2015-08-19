a4678ed12e4661004a8918b1be8a1456085d9a1d
ivon <xiaoyewen121@gmail.com>
2015-08-19 22:48:58 +0800

    保存作业

--- fis-conf.js
+++ fis-conf.js
@@ -1,13 +1,12 @@
 fis.config.merge({
     roadmap : {
-        // domain : "file:///C:/Users/Administrator/Desktop/website/4pit"
         domain: "http://ivonxiao.github.io/4pit"
     }
 });
 
-fis.config.set('roadmap.path',[
+/*fis.config.set('roadmap.path',[
 	{
 		reg: '**',
 		release: '/dist$&'
 	}
-]);
+]);*/
