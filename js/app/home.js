f0f774583aa9c5e2fb286febee27b1430cae695f
ivon <xiaoyewen121@gmail.com>
2015-08-26 18:07:03 +0800

    方案调整

--- js/app/home.js
+++ js/app/home.js
@@ -5,7 +5,7 @@ $(window).on('load',function() {
 		start: function() {
 			$('body').addClass('loaded');
 			$(".footer").css('visibility','visible');
-			/*图片懒加载*/
+			
 			$(".flexslider").find('img').each(function() {
 				var $self = $(this),
 					url = $self.data('src');
